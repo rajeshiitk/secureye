b6a6657b15bdf468197d4e709311958e04a4a8e1
Rajesh Choudhary <rajuvert18@gmail.com>
2024-01-15 14:17:41 +0530

    Add react-webcam and canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
+import Webcam from "react-webcam";
 
 interface Props {}
 
 const Page = (props: Props) => {
-  return <div>HomePage</div>;
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [mirrored, setMirrored] = useState(true);
+  return (
+    <div className="flex h-screen">
+      <div className="relative">
+        <div className="relative h-screen w-full">
+          <Webcam
+            ref={webcamRef}
+            mirrored={mirrored}
+            className="h-full w-full object-contain p-2"
+          />
+        </div>
+        <canvas
+          ref={canvasRef}
+          className="absolute top-0 left-0 h-full w-full object-contain p-2"
+        ></canvas>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
