f782515855ee7bc19c09b3389963144937a1d2c8
Rajesh Choudhary <rajuvert18@gmail.com>
2024-04-11 10:53:30 +0530

    testing

---
+++ src/app/temp-pages/adminPage.tsx
@@ -0,0 +1,338 @@
+"use client";
+import { useSocket } from "@/provider/socket-provider";
+import { useEffect, useState } from "react";
+import { useCallback } from "react";
+import peer from "@/utils/peer";
+import ReactPlayer from "react-player";
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
+import { Slider } from "@/components/ui/slider";
+import axios from "axios";
+import { SunIcon, ThermometerIcon } from "lucide-react";
+import { AdminSettingDrawerDialog } from "@/components/admin-setting";
+import { Rings } from "react-loader-spinner";
+
+// let base_url = "http://192.168.234.126:5000";
+
+const VideoStream = () => {
+  const { isConnected, socket } = useSocket(); // Access socket from context
+  const [message, setMessage] = useState("");
+  const [remoteSocketId, setRemoteSocketId] = useState("");
+  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
+  const [room, setRoom] = useState("1");
+
+  const [temperature, setTemperature] = useState(72);
+  const [humidity, setHumidity] = useState(50);
+  const [light, setLight] = useState(false);
+  const [ac, setAc] = useState(false);
+  const [fanSpeed, setFanSpeed] = useState(0);
+
+  const getSensorData = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_SITE_URL}/api/getsensordata`
+      );
+      setTemperature(response.data.temperature);
+      setHumidity(response.data.humidity);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // axios api to get temperature and humidity
+  useEffect(() => {
+    // interval to get temperature and humidity every 5 seconds
+    const interval = setInterval(() => {
+      getSensorData();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleJoinRoom = () => {
+    if (socket) {
+      console.log("Room Joined");
+
+      socket.emit("room:join", { room });
+    }
+  };
+
+  const handleUserJoined = useCallback(({ id }: { id: string }) => {
+    console.log(`id ${id} joined room`);
+    setRemoteSocketId(id);
+  }, []);
+
+  const handleIncommingCall = useCallback(
+    async ({ from, offer }: { from: any; offer: any }) => {
+      setRemoteSocketId(from);
+      console.log(`Incoming Call`, from, offer);
+      const ans = await peer.getAnswer(offer);
+      socket.emit("call:accepted", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const handleNegoNeeded = useCallback(async () => {
+    const offer = await peer.getOffer();
+    socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
+  }, [remoteSocketId, socket]);
+
+  useEffect(() => {
+    peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+    return () => {
+      peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
+    };
+  }, [handleNegoNeeded]);
+
+  const handleNegoNeedIncomming = useCallback(
+    async ({ from, offer }: { from: any; offer: any }) => {
+      const ans = await peer.getAnswer(offer);
+      socket.emit("peer:nego:done", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const handleNegoNeedFinal = useCallback(async ({ ans }: { ans: any }) => {
+    await peer.setLocalDescription(ans);
+  }, []);
+
+  useEffect(() => {
+    peer.peer.addEventListener("track", async (ev) => {
+      const remoteStream = ev.streams;
+      console.log("GOT TRACKS!!", remoteStream);
+      setRemoteStream(remoteStream[0]);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("user:joined", handleUserJoined);
+      socket.on("incomming:call", handleIncommingCall);
+      socket.on("peer:nego:needed", handleNegoNeedIncomming);
+      socket.on("peer:nego:final", handleNegoNeedFinal);
+
+      return () => {
+        socket.off("user:joined", handleUserJoined);
+        socket.off("incomming:call", handleIncommingCall);
+        socket.off("peer:nego:needed", handleNegoNeedIncomming);
+        socket.off("peer:nego:final", handleNegoNeedFinal);
+      };
+    }
+  }, [
+    socket,
+    handleIncommingCall,
+    handleUserJoined,
+    handleNegoNeedIncomming,
+    handleNegoNeedFinal,
+  ]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (data: any) => {
+        setMessage(data);
+        console.log("data : " + data);
+      });
+      socket.on("stream", (data: any) => {
+        // Handle video stream data
+        console.log("video-stream data : " + data);
+      });
+
+      // Handle cleanup on unmount
+      return () => socket.disconnect();
+    }
+  }, [socket]);
+
+  const sendMessage = () => {
+    console.log(socket);
+    if (!socket || !socket.connected) {
+      console.log("socket not connected");
+      return;
+    }
+    // if (!socket) return; // Handle empty message or missing socket
+    socket.emit("message", `Test Signal from Admin! ${socket.id}`);
+    setMessage("");
+    socket.on("error", (error: any) => {
+      console.log("error : " + error);
+    });
+    console.log("message sent");
+  };
+
+  const handleToggle = (checked: boolean) => {
+    setLight(checked);
+
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/light?status=${
+          checked ? "on" : "off"
+        }`
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log(checked);
+  };
+
+  const handleToggleAc = (checked: boolean) => {
+    setAc(checked);
+
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/ac?status=${
+          checked ? "on" : "off"
+        }`
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleFanSpeed = (speed: number) => {
+    setFanSpeed(speed);
+    axios
+      .get(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/fan?speed=${speed}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <AdminSettingDrawerDialog
+        sendMessage={sendMessage}
+        connectToSecurEye={handleJoinRoom}
+        acceptVideoStream={handleNegoNeeded}
+      />
+      <div>
+        <div className="fixed right-1 top-1">
+          {isConnected ? (
+            <Rings height={50} color="green" />
+          ) : (
+            <Rings height={50} color="red" />
+          )}
+        </div>
+        <div>
+          {" "}
+          <div className="grid w-full pt-16 px-2 m-auto h-[calc(100svh)] gap-4 md:gap-8 lg:grid-cols-[300px_1fr]">
+            <div className="grid gap-4 ">
+              <div className="grid gap-4">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Living Room</CardTitle>
+                    <CardDescription>2 devices</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="grid grid-cols-2 gap-4">
+                      <div className="flex items-center gap-2">
+                        <SunIcon className="w-6 h-6" />
+                        <span className="font-medium">Light</span>
+                        <Switch
+                          className="ml-auto w-10 h-6"
+                          checked={light}
+                          onCheckedChange={(checked) =>
+                            handleToggle(checked as boolean)
+                          }
+                        />
+                      </div>
+                      <div className="flex items-center gap-2">
+                        <ThermometerIcon className="w-6 h-6" />
+                        <span className="font-medium">AC</span>
+                        <Switch
+                          className="ml-auto w-10 h-6"
+                          checked={ac}
+                          onCheckedChange={(checked: boolean) =>
+                            handleToggleAc(checked as boolean)
+                          }
+                        />
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Fan Speed Control</CardTitle>
+                    <CardDescription>Adjust the Slider</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="grid gap-2">
+                      <div className="text-4xl font-semibold">{fanSpeed}</div>
+                      <Slider
+                        max={100}
+                        min={0}
+                        step={5}
+                        defaultValue={[fanSpeed]}
+                        onValueCommit={(val) => {
+                          setFanSpeed(val[0]);
+                          handleFanSpeed(val[0]);
+                        }}
+                      />{" "}
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Temperature</CardTitle>
+                    <CardDescription>Adjust the thermostat</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="grid gap-2">
+                      <div className="text-2xl font-semibold">
+                        Temperature: {temperature}
+                        <Slider value={[temperature]} className="w-full" />
+                      </div>
+                      <div className="text-2xl font-semibold">
+                        Humidity: {humidity}
+                        <Slider value={[humidity]} className="w-full" />
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="grid gap-4"></div>
+            </div>
+            <div className="flex flex-col gap-4">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Live Video</CardTitle>
+                  <CardDescription>Front Door</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="aspect-video rounded-xl overflow-hidden">
+                    {remoteStream && (
+                      <>
+                        <h1>Remote Stream</h1>
+                        <ReactPlayer
+                          playing
+                          muted
+                          height={"100%"}
+                          width={"100%"}
+                          url={remoteStream}
+                        />
+                      </>
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoStream;
---
+++ src/app/temp-pages/mainPage.tsx
@@ -0,0 +1,499 @@
+"use client";
+import { ModeToggle } from "@/components/themeToggle";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
+import {
+  Camera,
+  Cctv,
+  FlipHorizontal,
+  SwitchCameraIcon,
+  Video,
+  Volume2,
+} from "lucide-react";
+import { toast } from "sonner";
+import { Rings } from "react-loader-spinner";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Slider } from "@/components/ui/slider";
+import { beep } from "@/utils/audio";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs-backend-webgl";
+import "@tensorflow/tfjs-backend-cpu";
+import { Loader } from "@/components/loader";
+import { drawOnCanvas } from "@/utils/drawOnCanvas";
+import { formatDate } from "@/utils/formatDate";
+import { base64toBlob } from "@/utils/base64ToBlob";
+import Axios from "axios";
+import { useSocket } from "@/provider/socket-provider";
+import peer from "@/utils/peer";
+import { MainSettingDrawerDialog } from "@/components/main-setting";
+interface Props {}
+let interval: NodeJS.Timeout;
+let stopTimeout: NodeJS.Timeout;
+
+const Page = (props: Props) => {
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+  const [mirrored, setMirrored] = useState<boolean>(true);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecord, setAutoRecord] = useState<boolean>(false);
+  const [volume, setVolume] = useState<number>(0.7);
+  const [model, setModel] = useState<cocoSsd.ObjectDetection>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [camera, setCamera] = useState<"user" | "environment">("user");
+  const [Open, setOpen] = useState<boolean>(false);
+  const [isPersonDetected, setIsPersonDetected] = useState(false);
+  const [gateOpen, setGateOpen] = useState(false);
+  const [enableNodeMCU, setEnableNodeMCU] = useState(true);
+  const [myStream, setMyStream] = useState<MediaStream | null>(null);
+  const [remoteSocketId, setRemoteSocketId] = useState<string | null>();
+  const [room, setRoom] = useState<string>("1");
+  let noPersonTimeout: NodeJS.Timeout;
+
+  const videoConstraints = {
+    facingMode: camera === "user" ? "user" : { exact: "environment" },
+  };
+
+  const openGate = async () => {
+    try {
+      await Axios.get(`${process.env.NEXT_PUBLIC_NODEMCU_URL}/open`);
+      setGateOpen(true);
+    } catch (error) {
+      console.error("Error opening gate:", error);
+    }
+  };
+
+  const closeGate = async () => {
+    try {
+      await Axios.get(`${process.env.NEXT_PUBLIC_NODEMCU_URL}/close`);
+      setGateOpen(false);
+    } catch (error) {
+      console.error("Error closing gate:", error);
+    }
+  };
+
+  const { socket, isConnected } = useSocket();
+
+  const handleUserJoined = useCallback(({ id }: { id: string }) => {
+    console.log(`id ${id} joined room`);
+    setRemoteSocketId(id);
+  }, []);
+
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    const offer = await peer.getOffer();
+    socket.emit("user:call", { to: remoteSocketId, offer });
+    setMyStream(stream);
+    console.log("Call User");
+    console.log("Offer", offer);
+  }, [remoteSocketId, socket]);
+
+  const sendStreams = useCallback(() => {
+    console.log("Sending Streams");
+    if (myStream) {
+      for (const track of myStream.getTracks()) {
+        peer.peer.addTrack(track, myStream);
+      }
+    }
+  }, [myStream]);
+
+  const handleCallAccepted = useCallback(
+    ({ ans }: { ans: any }) => {
+      peer.setLocalDescription(ans);
+      console.log("Call Accepted!");
+      sendStreams();
+    },
+    [sendStreams]
+  );
+
+  const handleJoinRoom = () => {
+    if (socket) {
+      console.log("Room Joined");
+
+      socket.emit("room:join", { room });
+    }
+  };
+
+  const handleNegoNeeded = useCallback(async () => {
+    const offer = await peer.getOffer();
+    socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
+  }, [remoteSocketId, socket]);
+
+  useEffect(() => {
+    peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+    return () => {
+      peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
+    };
+  }, [handleNegoNeeded]);
+
+  const handleNegoNeedIncomming = useCallback(
+    async ({ from, offer }: { from: string; offer: any }) => {
+      const ans = await peer.getAnswer(offer);
+      socket.emit("peer:nego:done", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const handleNegoNeedFinal = useCallback(async ({ ans }: { ans: any }) => {
+    await peer.setLocalDescription(ans);
+  }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("user:joined", handleUserJoined);
+      socket.on("call:accepted", handleCallAccepted);
+      socket.on("peer:nego:needed", handleNegoNeedIncomming);
+      socket.on("peer:nego:final", handleNegoNeedFinal);
+
+      return () => {
+        socket.off("user:joined", handleUserJoined);
+        socket.off("call:accepted", handleCallAccepted);
+        socket.off("peer:nego:needed", handleNegoNeedIncomming);
+        socket.off("peer:nego:final", handleNegoNeedFinal);
+      };
+    }
+  }, [
+    socket,
+    handleCallAccepted,
+    handleUserJoined,
+    handleNegoNeedIncomming,
+    handleNegoNeedFinal,
+  ]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (data: any) => {
+        console.log("Received message:", data);
+      });
+      socket.on("stream", (data: any) => {
+        // Handle video stream data
+        console.log("video-stream data : " + data);
+      });
+      return () => {
+        socket.off("message", () => {
+          console.log("message off");
+        });
+        socket.off("stream", () => {
+          console.log("stream off");
+        });
+      };
+    }
+  }, [socket]);
+
+  useEffect(() => {
+    // If 'Open' state is true, open the gate
+    if (enableNodeMCU) {
+      if (isPersonDetected) {
+        openGate();
+      } else {
+        closeGate();
+      }
+    }
+  }, [isPersonDetected]);
+
+  useEffect(() => {
+    if (Open && enableNodeMCU) {
+      const res = Axios.get(`${process.env.NEXT_PUBLIC_NODEMCU_URL}/close`);
+      console.log(res);
+      setTimeout(() => {
+        closeGate();
+      }, 3000);
+    }
+  }, [Open]);
+
+  const toggleCamera = () => {
+    if (camera === "user") {
+      setCamera("environment");
+    } else {
+      setCamera("user");
+    }
+  };
+
+  useEffect(() => {
+    if (webcamRef && webcamRef.current && webcamRef.current?.video) {
+      const stream = (webcamRef.current.video as any).captureStream();
+      if (stream) {
+        mediaRecorderRef.current = new MediaRecorder(stream);
+
+        mediaRecorderRef.current.ondataavailable = (e) => {
+          if (e.data.size > 0) {
+            const recordedBlob = new Blob([e.data], { type: "video" });
+            const videoURL = URL.createObjectURL(recordedBlob);
+
+            const a = document.createElement("a");
+            a.href = videoURL;
+            a.download = `${formatDate(new Date())}.webm`;
+            a.click();
+          }
+        };
+        mediaRecorderRef.current.onstart = (e) => {
+          setIsRecording(true);
+        };
+        mediaRecorderRef.current.onstop = (e) => {
+          setIsRecording(false);
+        };
+      }
+    }
+  }, [webcamRef]);
+  // init model
+  useEffect(() => {
+    setIsLoading(true);
+    initModel();
+  }, []);
+
+  useEffect(() => {
+    if (model) {
+      setIsLoading(false);
+    }
+  }, [model]);
+
+  // load model
+  async function initModel() {
+    const loadedModel: cocoSsd.ObjectDetection = await cocoSsd.load({
+      base: "lite_mobilenet_v2",
+    });
+    setModel(loadedModel);
+  }
+
+  // run prediction
+  async function runPrediction() {
+    if (
+      model &&
+      webcamRef.current &&
+      webcamRef.current?.video &&
+      webcamRef.current?.video.readyState === 4
+    ) {
+      const predictions: cocoSsd.DetectedObject[] = await model.detect(
+        webcamRef.current?.video
+      );
+      resizeCanvas(canvasRef, webcamRef);
+      drawOnCanvas(mirrored, predictions, canvasRef.current?.getContext("2d"));
+      let isPerson: boolean = false;
+      if (predictions.length > 0) {
+        predictions.forEach((prediction) => {
+          isPerson = prediction.class === "person";
+          // console.log("prediction class :" + prediction.class);
+          // console.log(isPerson);
+        });
+
+        if (isPerson) {
+          setIsPersonDetected(true);
+          if (!gateOpen && enableNodeMCU) {
+            openGate();
+          }
+          // Restart the timeout if a person is detected
+          clearTimeout(noPersonTimeout);
+          noPersonTimeout = setTimeout(closeGate, 2000);
+        }
+
+        if (isPerson) {
+          setOpen(true);
+        }
+        if (isPerson && autoRecord) {
+          startRecording(true);
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      runPrediction();
+    }, 500);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [model, mirrored, webcamRef, autoRecord, camera]);
+
+  return (
+    <>
+      <MainSettingDrawerDialog
+        sendStream={handleCallUser}
+        enableNodeMCU={enableNodeMCU}
+        setEnableNodeMCU={setEnableNodeMCU}
+      />
+
+      <div className="fixed z-10 left-20 top-1">
+        {remoteSocketId && <p>Admin Socket: {remoteSocketId}</p>}
+      </div>
+      <div className="fixed right-1 top-1">
+        {isConnected ? (
+          <Rings height={50} color="green" />
+        ) : (
+          <Rings height={50} color="red" />
+        )}
+      </div>
+      <div className="flex-col flex md:flex-row relative h-[calc(100svh)]">
+        <div className="relative h-[calc(100svh)] w-full">
+          <div className="relative h-[calc(100svh)] w-full">
+            <Webcam
+              videoConstraints={videoConstraints}
+              ref={webcamRef}
+              mirrored={mirrored}
+              className="h-full w-full object-contain p-2"
+            />
+            <canvas
+              ref={canvasRef}
+              className="absolute top-0 left-0 h-full w-full object-contain p-2"
+            ></canvas>
+          </div>
+        </div>
+        {/* right side bar */}
+        <div className="border-primary/5  fixed bottom-0 w-full md:w-fit md:relative border-2 md:max-w-xs flex flex-row  md:flex-col gap-2 justify-between shadow-md rounded-md p-4">
+          <div className="flex flex-row md:flex-col gap-2">
+            <ModeToggle />
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal />
+            </Button>
+            <Separator className="hidden md:block md:my-2" />
+          </div>
+          <div className="flex flex-row md:flex-col gap-2">
+            <Separator className="hidden md:block md:my-2" />
+            <Button variant="outline" size="icon" onClick={toggleCamera}>
+              <SwitchCameraIcon />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={userPromptScreenshot}
+            >
+              <Camera />
+            </Button>
+            <Button
+              variant={isRecording ? "destructive" : "outline"}
+              size="icon"
+              onClick={userPromptRecord}
+            >
+              <Video />
+            </Button>
+            <Button
+              variant={autoRecord ? "destructive" : "outline"}
+              size="icon"
+              onClick={toggleAutoRecord}
+            >
+              {autoRecord ? <Rings color="white" height={45} /> : <Cctv />}
+            </Button>
+            <Separator className="hidden md:block md:my-2" />
+          </div>
+
+          <div className="flex flex-row md:flex-col gap-2">
+            <Separator className="hidden md:block md:my-2" />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.1}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0]);
+                    beep(val[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+        {isLoading && <Loader />}
+        {/* <div>
+          <button onClick={handleCallUser}>Send Stream</button>
+          <button onClick={handleJoinRoom}>Join Room</button>
+          {room && <p>Room: {room}</p>}
+          {socket ? <p>Connected to server</p> : <p>Not connected to server</p>}
+        </div> */}
+      </div>
+    </>
+  );
+
+  // handle function
+
+  function userPromptScreenshot() {
+    // take picture
+    if (!webcamRef.current) {
+      toast("Camera not found. Please refresh");
+    } else {
+      const imgSrc = webcamRef.current.getScreenshot();
+      console.log(imgSrc);
+      const blob = base64toBlob(imgSrc);
+
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `${formatDate(new Date())}.png`;
+      a.click();
+    }
+    // save it to downloads
+  }
+  function userPromptRecord() {
+    if (!webcamRef.current) {
+      toast("Camera is not found. Please refresh.");
+    }
+
+    if (mediaRecorderRef.current?.state === "recording") {
+      mediaRecorderRef.current.stop();
+      toast("Recording saved to downloads");
+      clearTimeout(stopTimeout);
+    } else {
+      startRecording(false);
+    }
+  }
+
+  function startRecording(doBeep: boolean) {
+    if (webcamRef.current && mediaRecorderRef.current?.state !== "recording") {
+      mediaRecorderRef.current?.start();
+      toast("Recording started");
+      doBeep && beep(volume);
+
+      stopTimeout = setTimeout(() => {
+        if (mediaRecorderRef.current?.state === "recording") {
+          mediaRecorderRef.current.stop();
+          toast("Recording saved to downloads");
+        }
+      }, 30000);
+    }
+  }
+  function toggleAutoRecord() {
+    if (autoRecord) {
+      setAutoRecord(false);
+      toast("Auto Record disabled");
+    } else {
+      setAutoRecord(true);
+      toast("Auto Record enabled");
+    }
+  }
+};
+
+export default Page;
+
+function resizeCanvas(
+  canvasRef: React.RefObject<HTMLCanvasElement>,
+  webcamRef: React.RefObject<Webcam>
+) {
+  const canvas = canvasRef.current;
+  const webcam = webcamRef.current?.video;
+  if (canvas && webcam) {
+    canvas.width = webcam.videoWidth;
+    canvas.height = webcam.videoHeight;
+  }
+}
